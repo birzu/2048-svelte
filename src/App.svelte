c5c672b8bf725b37efcb3566bec5a6f03621d5e1
subashmp <subash.mahapatra44@gmail.com>
2020-12-12 15:06:19 +0530

    feat: Display modal on win or loose condition and Start new game after game over

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
   import Board from "$components/board/Board.svelte";
   import GameScores from "./components/game/GameScores.svelte";
+  import Modal from "./components/UI/Modal.svelte";
   import Moves from "./components/UI/Moves.svelte";
   import { currentMove } from "./stores/currentMove";
+  import { currentScore } from "./stores/currentScore";
+  import type { TGState } from "./types";
 
   let title = "2048";
+  let reset = false;
+  let state: TGState = "RUNNING";
+  let result = "";
+
+  $: if (state === "OVER") {
+    result = "You Lost";
+  } else if (state === "WON") {
+    result = "You Won";
+  }
 </script>
 
 <style lang="scss" global>
@@ -29,6 +41,7 @@
     -moz-osx-font-smoothing: grayscale;
     font-family: "Titillium Web", sans-serif;
     font-weight: 400;
+    position: relative;
   }
 
   .game {
@@ -87,6 +100,52 @@
       }
     }
   }
+
+  .modal-root {
+    position: fixed;
+    background-color: rgba(#fff, 0.4);
+    backdrop-filter: blur(2px);
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    left: 0;
+  }
+
+  .game__result {
+    font-size: 4.4rem;
+    color: #776e65;
+    margin-bottom: 1.2rem;
+    font-weight: 400;
+  }
+
+  .game__scores {
+    width: 80%;
+    display: flex;
+    font-size: 2rem;
+    color: #776e65;
+    justify-content: center;
+    margin-bottom: 2.5rem;
+    & strong {
+      text-transform: uppercase;
+      font-size: 2rem;
+    }
+  }
+
+  .game__reset {
+    cursor: pointer;
+    padding: 1.2rem 2rem;
+    font-size: 1.8rem;
+    font-weight: 600;
+    color: #f6f9f1;
+    background-color: #655d55;
+    outline: none;
+    border: none;
+    border-radius: 0.3rem;
+
+    &:focus {
+      outline: 3px solid #f2b179;
+    }
+  }
 </style>
 
 <header>
@@ -104,7 +163,11 @@
 </header>
 <main>
   <section class="game">
-    <Board />
+    <Board
+      on:update-state={(e) => {
+        state = e.detail.state;
+      }}
+      shouldReset={reset} />
     <aside class="game-controls">
       <GameScores />
       <Moves currMove={$currentMove} />
@@ -124,3 +187,19 @@
     rel="noopener"
     target="_blank">github</a>
 </footer>
+{#if state === 'OVER' || state === 'WON'}
+  <div class="modal-root">
+    <Modal open={false}>
+      <h3 slot="header" class="game__result">{result}</h3>
+      <button
+        class="game__reset"
+        slot="actions"
+        on:click={() => (reset = true)}>New game</button>
+      <div class="game__scores" slot="content">
+        <div class="game__scores game__scores--curr">
+          <p><strong>Score: </strong><span>{$currentScore}</span></p>
+        </div>
+      </div>
+    </Modal>
+  </div>
+{/if}
---
+++ src/components/UI/Modal.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import { fade, fly, scale } from "svelte/transition";
+  import { cubicOut, linear } from "svelte/easing";
+  export let open: boolean;
+</script>
+
+<style lang="scss">
+  .modal {
+    box-sizing: border-box;
+    width: 65vh;
+    height: 30rem;
+    outline: none;
+    border: none;
+    border-radius: 0.4rem;
+    box-shadow: 0 4px 1.2rem 0 rgba($color: #000000, $alpha: 0.5);
+    padding: 2.5rem 4rem;
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    align-items: center;
+
+    &__content {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+    }
+  }
+</style>
+
+<dialog
+  {open}
+  class="modal"
+  in:scale={{ duration: 200, easing: cubicOut }}
+  out:fade={{ duration: 100, easing: linear }}>
+  <div class="modal__header">
+    <slot name="header" />
+  </div>
+  <div class="modal__content">
+    <slot name="content" />
+  </div>
+  <div class="modal__actions">
+    <slot name="actions" />
+  </div>
+</dialog>
